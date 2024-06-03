7de0d849f78182b0c4090614c0130eb32ec1cea8
erickgcia <erick17a@hotmail.com>
2024-06-03 14:36:29 -0600

    Remove secret from index-B43Sl4lI.js

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/translate-app/',
   plugins: [react()],
   test: {
     globals: true,
